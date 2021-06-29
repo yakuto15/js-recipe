1f8e8b165cb1e51c5a4247f96806c61d70cc7f29
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-06-30 02:24:53 +0900

    □と○を学びました

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,21 @@
+const figure = document.getElementById("figure")
+// const squareButton = document.getElementById("square-button")
+// const circleButton = document.getElementById("circle-button")
+
+// circleButton.onclick = function() {
+//   //figureにroundedクラスを追加する
+//   figure.classList.add("rounded")
+// }
+
+// squareButton.onclick = function() {
+//   //figureからroundedクラスを削除する
+//   figure.classList.remove("rounded")
+// }
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
+}
+
+// figure.mousemove = function() {
+//   figure.classList.toggle("rounded")
+// }
