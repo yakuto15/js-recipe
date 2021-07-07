d6308d0cf6e36dd20b90a71201d5879b02c2ca27
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-07-07 18:38:34 +0900

    [DofAPIで非同期処理]を学習しました

---
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -0,0 +1,13 @@
+const imageElement = document.getElementById("dog-image")
+const changePicture = document.getElementById("change-picture")
+
+changePicture.onclick = function() {
+  //指定したサーバーにデータを取りに行く
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => {
+      return res.json() //結果をjsonとして読み込んで、次のthenに渡す
+    })
+    .then((data) => {
+      imageElement.src = data.message //画像を表示する
+    })
+}
