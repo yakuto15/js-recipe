0443d645576b78f9bc981b9edb2815e4a535bb44
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-07-09 20:39:42 +0900

    エラーもかわいいのよ

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -8,6 +8,7 @@ changePicture.onclick = function() {
       return res.json() //結果をjsonとして読み込んで、次のthenに渡す
     })
     .then((data) => {
-      imageElement.src = data.message //画像を表示する
+      imageElement.src = da
+      ta.message //画像を表示する
     })
 }
