e27e4dd9b6b7b1ff6fcb6f65775500796f217536
yakuto15 <yakuto01058910@yahoo.co.jp>
2021-07-13 17:22:52 +0900

    ナベアツ

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -22,12 +22,18 @@ const genkiFunction = function(number) {
 }
 
 const FizzBuzz = function(number) {
+  //設定する数までループ処理
   for (let n = 1; n <= number; n++) {
+    //3の倍数と5の倍数両方当てはまるとき
     if (n % 3 === 0 && n % 5 === 0) {
       console.log("FizzBuzz")
+      //5の倍数
     } else if (n % 5 === 0) {
       console.log("Buzz")
+      //3の倍数
     } else if (n % 3 === 0) {
+      console.log("Fizz")
+    } else if (/3/.test(n)) {
       console.log("Fizz")
     } else {
       console.log(n)
